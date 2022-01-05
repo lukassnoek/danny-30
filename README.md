43d0b9d26128b1115c93fe0e759f4fdbd3d3ab7d
Lukas Snoek <lukassnoek@gmail.com>
2022-01-05 08:38:05 +0000

    Update instructions/download_model

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # danny-30
-Danny is turning 30!
+Danny is turning 30! Time for a puzzle.
+
+## Instructions
+
+1. Clone this repo
+2. Create a Python environment with Python version 3.9
+3. Install requirements (`pip install -r requirements.txt`)
+4. Download the model (`python download_model.py`)
+5. Check instructions (`python instructions.py`)
+6. Good luck!
+
--- download_model.py
+++ download_model.py
@@ -1,8 +1,43 @@
-import io
-import zipfile
+import os
 import requests
+from zipfile import ZipFile
 
-url = 'test'
-r = requests.get(url)
-z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall()
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    
+    print("Downloading DannyNet ...")
+    file_id = '1BRy7p6_-V3RGa12gTr8zEyTnJ1wwG2z4'
+    download_file_from_google_drive(file_id, 'DannyNet.zip')
+    with ZipFile('DannyNet.zip') as zf:
+        zf.extractall('./')
+        
+    os.remove('DannyNet.zip')
+    
